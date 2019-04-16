2991a54a73182167dececfc1e614dc1911f9ce07
ngagearun <45345193+ngagearun@users.noreply.github.com>
2019-04-16 07:29:21 +0530

    Add files via upload

---
+++ ReturnType.java
@@ -0,0 +1,22 @@
+
+public class ReturnType {
+	
+	int stdrollno=1001;
+	
+	 int show(){
+		 //System.out.println(stdrollno);
+		 return stdrollno;
+	 }
+	 
+	 void display(){
+		 
+	 }
+	public static void main(String[] args) {
+		
+		ReturnType ret=new ReturnType();
+		int b=ret.show();
+		
+
+	}
+
+}
